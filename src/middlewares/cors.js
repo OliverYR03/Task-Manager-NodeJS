aef1bd3eea55d6a480c9467a6d5f6e59efd987c8
OliverYR03 <oliver_del22@hotmail.com>
2025-02-09 12:47:59 -0500

    origins modified

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,6 +2,8 @@ import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
+    'https://task-manager-nodejs-bvjh.onrender.com/',
+    'https://task-manager-react-iz4u.onrender.com/',
     'http://localhost:1234',
     'http://localhost:5173'
 
